4b8f349e6d85e2a9b6f4b5e49dbce7145e6be582
Edw1rdt12 <edward.mancero@uisek.edu.ec>
2025-12-16 11:20:56 -0500

    Conexión con Django

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Header } from './components/Header'
 import { Container } from '@mui/material'
 import './App.css'
 import PokemonList from './components/PokemonList'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import PokemonForm from './components/PokemonForm'
 
 
 function App() {
@@ -9,10 +11,17 @@ function App() {
     <>
       <Header />
       <Container>
-          <PokemonList />
+        <BrowserRouter>
+          {/*Aqui irán las rutas*/}
+          <Routes>
+            <Route path="/" element = {<PokemonList/>} />
+            <Route path="/add-pokemon" element = {<PokemonForm/>} />
+          </Routes>
+          </BrowserRouter>
       </Container>
     </>
   )
 }
 
 export default App
+
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { AppBar, Container, Toolbar } from "@mui/material";
+import { AppBar, Button, Container, Toolbar } from "@mui/material";
 import pokedexLogo from "../assets/pokedex.png";
 import "./Header.css";
 
@@ -11,6 +11,12 @@ export function Header(){
                        <div className="image-container">
                             <img src={pokedexLogo} alt="Logo" height={100} />
                        </div>
+                 </Toolbar>
+                 <Toolbar >
+                    
+                        <Button color="inherit" href="/">Inicio</Button>
+                        <Button color="inherit" href="/add-pokemon">Agregar Pokemon</Button>
+
                  </Toolbar>
              </AppBar>
          </div>
